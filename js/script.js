708d085b4c276eb99070df5a019b6e20a7374c7b
Phan Trung Nguyên <nguyenpl117@gmail.com>
2025-01-09 17:23:43 +0700

    update

--- js/script.js
+++ js/script.js
@@ -5,79 +5,99 @@
     //     cellAlign: 'left',
     //     contain: true
     // });
-    document.querySelector('.btndown').addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(e.target.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
-    });
+    if (document.querySelector('.btndown')) {
+        document.querySelector('.btndown').addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(e.target.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+        });
+    }
 
-    new Flickity('.home-carousel', {
-        cellAlign: 'left',
-        contain: true,
-        prevNextButtons: false,
-        autoPlay: 3000,
-        pageDots: true
-    });
-
-    // element argument can be a selector string
-    //   for an individual element
-    const flkty = new Flickity( '.main-carousel', {
-        cellAlign: 'left',
-        contain: true,
-        prevNextButtons: false,
-        autoPlay: 3000,
-    });
+    if (document.querySelector('.home-carousel')) {
+        new Flickity('.home-carousel', {
+            cellAlign: 'left',
+            contain: true,
+            prevNextButtons: false,
+            autoPlay: 3000,
+            pageDots: true
+        });
+    }
 
 
-    new Flickity( '.project-carousel', {
-        cellAlign: 'left',
-        contain: true,
-        prevNextButtons: false,
-    });
+    if (document.querySelector('.main-carousel')) {
+        // element argument can be a selector string
+        //   for an individual element
+        const flkty = new Flickity('.main-carousel', {
+            cellAlign: 'left',
+            contain: true,
+            prevNextButtons: false,
+            autoPlay: 3000,
+        });
+    }
+
+    if (document.querySelector('.project-carousel')) {
+        new Flickity( '.project-carousel', {
+            cellAlign: 'left',
+            contain: true,
+            prevNextButtons: false,
+        });
+    }
+    if (document.querySelector('.project-carousel2')) {
+        new Flickity( '.project-carousel2', {
+            cellAlign: 'left',
+            contain: true,
+            prevNextButtons: false,
+        });
+    }
 
     const homeElm = document.querySelector('.home-carousel');
-    const resizeHomeObserver = new ResizeObserver((entries) => {
-        for (const entry of entries) {
-            const { width, height } = entry.contentRect;
-            // console.log(width, height)
-        }
-    });
-    resizeHomeObserver.observe(homeElm);
-
-    const divElm = document.querySelector('.partners');
-    const resizeObserver = new ResizeObserver((entries) => {
-        requestAnimationFrame(() => {
+    if (homeElm) {
+        const resizeHomeObserver = new ResizeObserver((entries) => {
             for (const entry of entries) {
-                const { width } = entry.contentRect;
-                switch (true) {
-                    case width < 550:
-                        new Flickity('.partners-carousel', {
-                            cellAlign: 'left',
-                            contain: true,
-                            prevNextButtons: false,
-                            groupCells: 1,
-                        });
-                        break;
-                    case width < 1024:
-                        new Flickity('.partners-carousel', {
-                            cellAlign: 'left',
-                            contain: true,
-                            prevNextButtons: false,
-                            groupCells: 3,
-                        });
-                        break;
-                    default:
-                        new Flickity('.partners-carousel', {
-                            cellAlign: 'left',
-                            contain: true,
-                            prevNextButtons: false,
-                            groupCells: 4,
-                        });
-                        break;
-                }
+                const { width, height } = entry.contentRect;
+                // console.log(width, height)
             }
         });
-    });
-    resizeObserver.observe(divElm);
+        resizeHomeObserver.observe(homeElm);
+    }
+
+
+    const divElm = document.querySelector('.partners');
+    if (divElm) {
+        const resizeObserver = new ResizeObserver((entries) => {
+            requestAnimationFrame(() => {
+                for (const entry of entries) {
+                    const { width } = entry.contentRect;
+                    switch (true) {
+                        case width < 550:
+                            new Flickity('.partners-carousel', {
+                                cellAlign: 'left',
+                                contain: true,
+                                prevNextButtons: false,
+                                groupCells: 1,
+                            });
+                            break;
+                        case width < 1024:
+                            new Flickity('.partners-carousel', {
+                                cellAlign: 'left',
+                                contain: true,
+                                prevNextButtons: false,
+                                groupCells: 3,
+                            });
+                            break;
+                        default:
+                            new Flickity('.partners-carousel', {
+                                cellAlign: 'left',
+                                contain: true,
+                                prevNextButtons: false,
+                                groupCells: 4,
+                            });
+                            break;
+                    }
+                }
+            });
+        });
+        resizeObserver.observe(divElm);
+    }
 
     document.addEventListener('scroll', function (e) {
         if (e.currentTarget.scrollingElement.scrollTop > 50) {
